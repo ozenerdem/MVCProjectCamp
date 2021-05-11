308aaf12828de15efe051efc995a85f8981e40d6
ozenerdem <ozenerdem98@gmail.com>
2021-05-11 19:51:58 +0300

    CRUD operations done

--- MVCProjectCamp/BusinessLayer/Concrete/CategoryManager.cs
+++ MVCProjectCamp/BusinessLayer/Concrete/CategoryManager.cs
@@ -12,21 +12,36 @@ namespace BusinessLayer.Concrete
 {
     public class CategoryManager : ICategoryService
     {
-        ICategoryDal _categorydal;
+        ICategoryDal _categoryDal;
 
-        public CategoryManager(ICategoryDal categorydal)
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            _categorydal = categorydal;
+            _categoryDal = categoryDal;
         }
 
         public void CategoryAdd(Category category)
         {
-            _categorydal.Insert(category);
+            _categoryDal.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+
+        public Category GetByID(int id)
+        {
+            return _categoryDal.Get(x => x.CategoryID == id);
         }
 
         public List<Category> GetList()
         {
-            return _categorydal.List();
+            return _categoryDal.List();
         }
 
 
